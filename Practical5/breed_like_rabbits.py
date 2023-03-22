84acf52d99ac6d39b1f43b12009eadb69a9b0bd9
wenzhuo <wenzhuo.22@intl.zju.edu.cn>
2023-03-22 12:29:18 +0100

    initialise

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,6 @@
+n = 2                     #start with 2 rabbits
+g = 1                     #start with generation 1
+while n <= 100:
+ n = 2*n
+ g += 1                   #the number of rabbits double every generation               
+print (g)          
---
+++ Practical5/variables.py
@@ -0,0 +1,9 @@
+a = -3.19
+b = -118.24
+c = 116.39
+d = a-b
+e = a-c
+if d > e:
+ print ( "Rob travel futher to Los Angeles")
+else:
+ print ("Rob travel futher to Haining" )
